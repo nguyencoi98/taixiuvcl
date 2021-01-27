f54a62b675c72bf13c086463496c016a04cad795
Glitch (vintage-agate-temperature) <none>
2021-01-27 12:16:05 +0000

    🍫👖 Checkpoint ./server.js:26111746/7

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var io = socketIO(server,
 });
 var cuoc   = Array();
 var system = {
-	url : 'https://nro2021.club', 
+	url : 'https://nro2021.com', 
 	keycode : '34545446hhhh', 
 	ngocrong_min : 1000, 
 	ngocrong_max : 2000,
