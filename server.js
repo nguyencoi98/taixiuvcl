df7ce7989d11f546903f1b5cd060961fd0084744
Glitch (vintage-agate-temperature) <none>
2021-10-06 06:18:57 +0700

    make it better

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var io = socketIO(server,
 });
 var cuoc   = Array();
 var system = {
-	url : 'https://rikvip66.club', 
+	url : 'https://game.ngocrongonline.club', 
 	keycode : '123456', 
 	ngocrong_min : 1000, 
 	ngocrong_max : 2000,
